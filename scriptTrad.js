7c83f3416102c0bd079c63f521d5d98629640698
Bendufgit <benjamin.dufils@hotmail.fr>
2019-06-28 15:38:56 +0200

    fin du sprint 3 , retour API tableau

--- scriptTrad.js
+++ scriptTrad.js
@@ -1,7 +1,28 @@
 $(function(){
 
-var text= $(".text").text();
+// var text= $(".text").text();
+// console.log(text);
+
+// n = $(".text").length;
+// console.log(n);
+
+
+
+var text = [];
+$('.text').each(function(){
+    text.push($(this).text());
+});
 console.log(text);
+// console.log(Array.from(text));
+// function lire2(tab)
+// {
+//         var chaine = "Le tableau contient :";
+//         for(var indice in tab)
+//                 chaine += "\n" + indice + " -> " + tab[indice];
+//         return chaine;
+// }
+
+
 
 
 $("#button1").on("click",function AppelApi(){
@@ -28,29 +49,34 @@ $("#trad").empty();
  })
 });
 
-$("#button2").on("click",function AppelApi(){
- $.get("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=fr-ru&text="+text+"&key=trnsl.1.1.20190627T094614Z.78f5ccb3c2153c6a.8328e723a3c4192d5572b28517b553864927df1a&[format=html]",callbackGetSuccess)
-.done(function(){
- })
-.fail(function(){
- alert("ca marche pas, fail!!");
- })
-.always(function(){
- ;
- })
-});
 
-$("#button2").on("click",function AppelApi(){
- $.get("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=fr-sp&text="+text+"&key=trnsl.1.1.20190627T094614Z.78f5ccb3c2153c6a.8328e723a3c4192d5572b28517b553864927df1a&[format=html]",callbackGetSuccess)
-.done(function(){
- })
-.fail(function(){
- alert("ca marche pas, fail!!");
- })
-.always(function(){
- ;
- })
-});
+
+
+
+
+// $("#button2").on("click",function AppelApi(){
+//  $.get("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=fr-ru&text="+text+"&key=trnsl.1.1.20190627T094614Z.78f5ccb3c2153c6a.8328e723a3c4192d5572b28517b553864927df1a&[format=html]",callbackGetSuccess)
+// .done(function(){
+//  })
+// .fail(function(){
+//  alert("ca marche pas, fail!!");
+//  })
+// .always(function(){
+//  ;
+//  })
+// });
+
+// $("#button2").on("click",function AppelApi(){
+//  $.get("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=fr-sp&text="+text+"&key=trnsl.1.1.20190627T094614Z.78f5ccb3c2153c6a.8328e723a3c4192d5572b28517b553864927df1a&[format=html]",callbackGetSuccess)
+// .done(function(){
+//  })
+// .fail(function(){
+//  alert("ca marche pas, fail!!");
+//  })
+// .always(function(){
+//  ;
+//  })
+// });
 
 let callbackGetSuccess = function(data) {
 
@@ -58,9 +84,13 @@ let callbackGetSuccess = function(data) {
  $(".text").append(data.text);
  
  console.log(data);
- 
+ console.log(data.text[0].split(','));
 };
 });
+
+
+
+
 // Fonction Receive du selecteur de langue 
 
 // function receive()
