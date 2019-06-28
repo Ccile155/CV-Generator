966c139c1c80b8fb67b776b64c63abd329c7f1c0
Bendufgit <benjamin.dufils@hotmail.fr>
2019-06-28 13:41:24 +0200

    Fin du sprint 2 selecteur

--- scriptTrad.js
+++ scriptTrad.js
@@ -4,8 +4,44 @@ var text= $(".text").text();
 console.log(text);
 
 
-$("#button").on("click",function AppelApi(){
- $.get("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=fr-en&text="+text+"&key=trnsl.1.1.20190627T094614Z.78f5ccb3c2153c6a.8328e723a3c4192d5572b28517b553864927df1a&[format=html]",callbackGetSuccess)
+$("#button1").on("click",function AppelApi(){
+
+ var parameters = location.search.substring(1).split("&");
+  var temp = parameters[0].split("=");
+  varname = unescape(temp[0]);
+  if(varname == "") return;
+  value = unescape(temp[1]);
+ //  // alert(varname + "=" + value);
+
+// var selected1 = document.getElementById("langue").value;
+
+ $.get("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=fr-"+value+"&text="+text+"&key=trnsl.1.1.20190627T094614Z.78f5ccb3c2153c6a.8328e723a3c4192d5572b28517b553864927df1a&[format=plain]",callbackGetSuccess)
+.done(function(){
+$("#trad").empty();
+ $("#trad").append("le texte est traduit en " + value);
+ })
+.fail(function(){
+ alert("ca marche pas, fail!!");
+ })
+.always(function(){
+ ;
+ })
+});
+
+$("#button2").on("click",function AppelApi(){
+ $.get("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=fr-ru&text="+text+"&key=trnsl.1.1.20190627T094614Z.78f5ccb3c2153c6a.8328e723a3c4192d5572b28517b553864927df1a&[format=html]",callbackGetSuccess)
+.done(function(){
+ })
+.fail(function(){
+ alert("ca marche pas, fail!!");
+ })
+.always(function(){
+ ;
+ })
+});
+
+$("#button2").on("click",function AppelApi(){
+ $.get("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=fr-sp&text="+text+"&key=trnsl.1.1.20190627T094614Z.78f5ccb3c2153c6a.8328e723a3c4192d5572b28517b553864927df1a&[format=html]",callbackGetSuccess)
 .done(function(){
  })
 .fail(function(){
@@ -21,9 +57,18 @@ let callbackGetSuccess = function(data) {
  $(".text").empty();
  $(".text").append(data.text);
  
- console.log(data.text);
+ console.log(data);
  
 };
-
-
 });
+// Fonction Receive du selecteur de langue 
+
+// function receive()
+// {
+//   var parameters = location.search.substring(1).split("&");
+//   var temp = parameters[0].split("=");
+//   varname = unescape(temp[0]);
+//   if(varname == "") return;
+//   value = unescape(temp[1]);
+//   alert(varname + "=" + value);
+// }
